636e3b48c049274e9cd677b0f415e556355f1b15
xuyao <xuyao@zbj.com>
2019-10-15 11:29:20 +0800

    代码提交。登录模块

---
+++ src/main/java/com/cy/home/controller/LoginController.java
@@ -0,0 +1,13 @@
+package com.cy.home.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/login")
+public class LoginController {
+
+    public
+
+
+}
